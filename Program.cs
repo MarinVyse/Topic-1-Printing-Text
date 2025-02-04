b7044b9c2d9d807d44b9b36d7858cfb7dbea9712
362587859 <362587859@2260I-L29-026>
2025-02-04 14:01:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Topic_1_Printing_Text
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Marin Vyse
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine(" This will print on a new line");
+
+            Console.WriteLine();
+
+            Console.Write("Hello World");
+            Console.Write("This will print on the same line");
+
+
+
+
+        }
+    }
+}
